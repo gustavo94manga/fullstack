fea8f73d3bb308f9291f96b14d8eefaeeeb6448c
Angel Garza <gustavo94manga@gmail.com>
2023-10-31 18:08:00 -0500

    add registration form checks

--- frontend/src/app/login-form/login-form.component.ts
+++ frontend/src/app/login-form/login-form.component.ts
@@ -1,35 +1,42 @@
-import { EventEmitter, Component, Output } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.css'],
-  })
+})
 export class LoginFormComponent {
-
   @Output() onSubmitLoginEvent = new EventEmitter();
   @Output() onSubmitRegisterEvent = new EventEmitter();
 
-	active: string = "login";
+  active: string = "login";
   firstName: string = "";
   lastName: string = "";
   login: string = "";
   password: string = "";
 
-	onLoginTab(): void {
-		this.active = "login";
-	}
+  onLoginTab(): void {
+    this.active = "login";
+  }
 
-	onRegisterTab(): void {
-		this.active = "register";
-	}
+  onRegisterTab(): void {
+    this.active = "register";
+  }
 
   onSubmitLogin(): void {
-    this.onSubmitLoginEvent.emit({"login": this.login, "password": this.password});
+    this.onSubmitLoginEvent.emit({ "login": this.login, "password": this.password });
   }
 
   onSubmitRegister(): void {
-    this.onSubmitRegisterEvent.emit({"firstName": this.firstName, "lastName": this.lastName, "login": this.login, "password": this.password});
+    if (this.firstName && this.lastName && this.login && this.password) {
+      this.onSubmitRegisterEvent.emit({
+        "firstName": this.firstName,
+        "lastName": this.lastName,
+        "login": this.login,
+        "password": this.password
+      });
+    } else {
+      alert("All fields are required!");
+    }
   }
-
 }
